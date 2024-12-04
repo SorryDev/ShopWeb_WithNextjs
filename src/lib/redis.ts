895e7dbe0277ab913065588005b9fa1f8d065964
SorryDev <109810292+SorryDev@users.noreply.github.com>
2024-12-04 12:32:45 +0700

    Update redis.ts

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,8 +1,16 @@
 import { Redis } from '@upstash/redis'
 
+if (!process.env.KV_REST_API_URL) {
+  throw new Error('KV_REST_API_URL is not defined')
+}
+
+if (!process.env.KV_REST_API_TOKEN) {
+  throw new Error('KV_REST_API_TOKEN is not defined')
+}
+
 const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL!,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+  url: process.env.KV_REST_API_URL,
+  token: process.env.KV_REST_API_TOKEN,
 })
 
 export default redis
